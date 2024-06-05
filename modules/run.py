03672ccc4d70a71cad6f40fb1c606335315f63bf
Rifqi Alfaizi <rifqi.alfaizi@phincon.com>
2024-06-06 00:09:56 +0800

    add exeption handling

--- modules/run.py
+++ modules/run.py
@@ -2,9 +2,17 @@ from utils.menu import menu
 
 from modules.user import user
 
-def check_expression(expression):
-    return None
-
+def check_expression(expression, option):
+    if option == 1:
+        if " " in expression:
+            print("wrong input format")
+            return False
+    elif option == 2:
+        sheetName = expression.split(" ")
+        if len(sheetName) != 2:
+            print("wrong input format")
+            return False
+    return True
 
 def run():
     users = {}
@@ -16,15 +24,17 @@ def run():
             break
         elif data == '1':
             userName = input("> ")
-            users[userName] = user(userName)
+            if check_expression(userName, 1):
+                users[userName] = user(userName)
         elif data == '2':
             sheetName = input("> ")
-            userName,sheetName = sheetName.split(' ')
-            if userName in users:
-                users[userName].create_sheet(sheetName)
-                print(f"Create a sheet named \"{sheetName} \" for \"{userName}\"")
-            else:
-                print(f"User \"{userName}\" does not exist")
+            if check_expression(sheetName, 2):
+                userName,sheetName = sheetName.split(' ')
+                if userName in users:
+                    users[userName].create_sheet(sheetName)
+                    print(f"Create a sheet named \"{sheetName} \" for \"{userName}\"")
+                else:
+                    print(f"User \"{userName}\" does not exist")
         elif data == '3':
             sheetName = input("> ")
             userName,sheetName = sheetName.split(' ')
@@ -37,7 +47,6 @@ def run():
                     print(f"Sorry, sheet \"{sheetName}\" does not exist")
             else:
                 print(f"User \"{userName}\" does not exist")
-        
         else:
             print("Please enter a number")
 
